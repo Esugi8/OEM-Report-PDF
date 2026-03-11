f97409e960c0565d1cb6f97315c793917dd9e028
Esugi8 <mamo.1991.esugi@gmail.com>
2026-03-11 18:32:19 +0900

    Update Renketsu.py

--- Renketsu.py
+++ Renketsu.py
@@ -62,7 +62,7 @@ def process_pdf(uploaded_file):
     """
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash-lite",
+        model="gemini-3.1-flash-lite-preview",
         contents=[gemini_file, prompt],
         config=types.GenerateContentConfig(
             response_mime_type="application/json",
